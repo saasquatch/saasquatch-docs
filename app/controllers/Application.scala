e57c88b0288c13845bd7092d9a74619e3d477599
Logan <logan.volkers@yupiq.com>
2013-12-31 17:30:02 -0500

    LV: Added a couple example pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,18 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+    
+  val slugMap = HashMap(
+        "testing-best-practices" -> views.html.index.testing-best-practices,
+        "page-speed-best-practices" ->  views.html.index.page-speed-best-practices
+      )
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+  
+  def page(slug){
+    Ok(slugMap.get(slug))
   }
 
 }
