47f77d426564332c84d03a2b6c430ad1b708add1
Logan <logan.volkers@yupiq.com>
2013-12-31 20:27:13 -0500

    LV: Migrated old docs in

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,14 +6,30 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
   
   def page(slug:String) = Action {
     slug match {
-        case "testing-best-practices"  => Ok(views.html.testingBestPractices())
-        case "page-speed-best-practices" => Ok(views.html.pageSpeedBestPractices())
-        case "braintree" => Ok(views.html.braintree())
+        case "how-it-works"  => Ok(views.html.howItWorks())
+        
+        case "braintree"  => Ok(views.html.braintree())
+        case "recurly"  => Ok(views.html.recurly())
+        case "stripe"  => Ok(views.html.stripe())
+        case "zuora"  => Ok(views.html.zuora())
+        
+        case "squatchjs"  => Ok(views.html.squatchjs())
+        
+        case "bestpractice/buttons"  => Ok(views.html.buttonsBestPractices())
+        case "bestpractice/speed"  => Ok(views.html.pageSpeedBestPractices())
+        case "bestpractice/testing"  => Ok(views.html.testingBestPractices())
+        
+        case "faq"  => Ok(views.html.faqGeneral())
+        case "faq/pricing"  => Ok(views.html.faqPricing())
+        
+        case "getting-started"  => Redirect("/")
+        case "faq/general"  => Redirect("/faq")
+
         case _ => NotFound
     }
   }
