bcac2c7de3294bf50e529aa4bd263048a848de8b
Logan <logan.volkers@yupiq.com>
2014-01-02 16:54:31 -0500

    LV: Updated error handler for play 2.2.1

--- app/Global.scala
+++ app/Global.scala
@@ -3,7 +3,10 @@ import play.api.mvc._
 import play.api.mvc.Results._
 
 object Global extends GlobalSettings {
-  override def onHandlerNotFound(request: RequestHeader): Result = {
-    NotFound(views.html.notFound())
+
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(
+      views.html.notFoundPage()
+    ))
   }
 }
