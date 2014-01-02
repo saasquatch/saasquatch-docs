19cddbc794d31580f305ed7446b292595e665df9
Logan <logan.volkers@yupiq.com>
2014-01-02 16:39:02 -0500

    LV: Added missing imports to Global

--- app/Global.scala
+++ app/Global.scala
@@ -1,3 +1,6 @@
+import play.api._
+import play.api.mvc._
+
 object Global extends GlobalSettings {
   override def onHandlerNotFound(request: RequestHeader): Result = {
     NotFound(views.html.notFound())
