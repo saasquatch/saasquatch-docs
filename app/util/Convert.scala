f5d08426509bc764ec292c9e01950a0f3f264af4
Logan <logan.volkers@yupiq.com>
2014-11-05 03:32:31 +0000

    LV: Moved markdown conversion into it's own thing. Added markdown processing to issue.message

--- app/util/Convert.scala
+++ app/util/Convert.scala
@@ -5,9 +5,6 @@ object Convert {
     
     val pegdown = new org.pegdown.PegDownProcessor()
     
-    def markdownToHtml(rawMd: String){
-        val htmlStr = pegdown.markdownToHtml(rawMd)
-        play.api.templates.Html(htmlStr)
-    }
-    
+    def markdownToHtml(rawMd: String) = play.api.templates.Html(pegdown.markdownToHtml(rawMd))
+
 }
