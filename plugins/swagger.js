5f27527a77982e0943ec87d51dad6d288425895f
Logan <logan.volkers@yupiq.com>
2015-02-18 02:58:07 +0000

    LV: Added basic Swagger documentation auto-generation

---
+++ plugins/swagger.js
@@ -0,0 +1,58 @@
+var extname = require('path').extname;
+var parser = require("swagger-parser");
+var yaml = require('js-yaml');
+
+/**
+ * Expose `plugin`.
+ */
+
+module.exports = plugin;
+
+var parsers = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad
+};
+
+/**
+ * Metalsmith plugin to process Swagger.io files and add them as dereferenced output
+ *
+ * @param {Object} opts
+ * @return {Function}
+ */
+
+function plugin(opts){
+  opts = opts || {};
+
+  return function(files, metalsmith, done){
+    var metadata = metalsmith.metadata();
+    var exts = Object.keys(parsers);
+
+    var file = opts.path;
+    var ext = extname(file);
+    if (!~exts.indexOf(ext)) throw new Error('unsupported metadata type "' + ext + '"');
+    if (!files[file]) throw new Error('file "' + file + '" not found');
+    
+    var parse = parsers[ext];
+    var str = files[file].contents.toString();
+
+    // Different than `metalsmith-metadata` (we want to leave our lovely JSON files for future donwloadability)
+    // delete files[file]; 
+    
+    try {
+        var data = parse(str);
+    } catch (e) {
+        return done(new Error('malformed data in "' + file + '"'));
+    }   
+
+    parser.parse(data, function(err, api, swaggerMetaD) {
+        if(err){
+            return done(new Error("Swagger parsing error: " + err.message, err));
+        }
+    
+        metadata['swagger'] = api;
+    
+        done();
+    });
+
+  };
+}
