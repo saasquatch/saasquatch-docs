e5d21c9e7c08a0a7917334fd4c5c9f0b7aecbe32
Logan <logan.volkers@yupiq.com>
2015-04-02 21:12:40 +0000

    Revert "LV: Removed in-progress google custom search"
    This reverts commit 1a381098321c86f9d0edc585601f8e6a8399382e.

--- src/assets/docs.js
+++ src/assets/docs.js
@@ -28,5 +28,45 @@ jQuery(document).ready(function() {
     
     jQuery("#js-location-path").text(document.location.pathname);
 
+    if(Handlebars){
+        (function(){
+            var template = Handlebars.compile(jQuery("#search-template").html());
+            
+            function getParameterByName(name) {
+                name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                    results = regex.exec(location.search);
+                return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+            }
+            
+            var query = getParameterByName('q');
+            
+            jQuery.ajax({
+                url: "https://www.googleapis.com/customsearch/v1",
+                
+                // The name of the callback parameter, as specified by the YQL service
+                jsonp: "callback",
+             
+                // Tell jQuery we're expecting JSONP
+                dataType: "jsonp",
+             
+                // Tell YQL what we want and that we want JSON
+                data: {
+                    q: query,
+                    cx: "014638356218796023717:gvlcaiusvsk",
+                    key: "AIzaSyCMGfdDaSfjqv5zYoS0mTJnOT3e9MURWkU",
+                    format: "json"
+                },
+             
+                // Work with the response
+                success: function( response ) {
+                    jQuery('#results').html(JSON.stringify(response, " ", "\t"));
+                    var html = template(response);
+                    jQuery('#pretty-results').html(html);
+        
+                }
+            });
+        })();
+    }
 
 });
