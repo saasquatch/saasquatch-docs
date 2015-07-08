35a96bb4c1e74c926477ec445754f7fb88a11a52
Logan <logan@saasquat.ch>
2015-07-08 23:01:57 +0000

    LV: Added feedback Typeform to docs

--- src/assets/docs.js
+++ src/assets/docs.js
@@ -59,8 +59,42 @@ jQuery(document).ready(function() {
         return false; // prevent default
     });
 
-    // TODO: Use JQuery to selectively load
+
+    /**
+     *  Typeform Javascript
+     * 
+     *  Finds all 
+     */
+
+    (function() {
+        var js, q, d = document,
+          gi = d.getElementById,
+          ce = d.createElement,
+          gt = d.getElementsByTagName,
+          id = 'typef_orm',
+          b = 'https://s3-eu-west-1.amazonaws.com/share.typeform.com/';
+        if (!gi.call(d, id)) {
+          js = ce.call(d, 'script');
+          js.id = id;
+          js.src = b + 'share.js';
+          q = gt.call(d, 'script')[0];
+          q.parentNode.insertBefore(js, q);
+        }
+        id = id + '_';
+        if (!gi.call(d, id)) {
+        }
+    })();
     
+    
+    /**
+     * 
+     *  Docs Search
+     * 
+     * 
+     *  Uses google custom search, Handlebars, and format 
+     * 
+     */
+
     jQuery("#js-docs-search-results").each(function(){
         
         
