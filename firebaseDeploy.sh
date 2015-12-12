383fd0b4cdad37308a012ce0b2454b8c1e806136
Logan <logan@saasquat.ch>
2015-12-12 01:07:16 +0000

    LV: Removed unfriendly output

--- firebaseDeploy.sh
+++ firebaseDeploy.sh
@@ -3,10 +3,6 @@ set -ev
 
 echo "Starting firebase scripts"
 
-echo "Confirming auth"
-firebase list --token "$FIREBASETOKEN" || echo "Error confirming auth"
-
-echo "Deploying site"
 firebase deploy --token "$FIREBASETOKEN" || echo "Error deploying site"
 
-echo "Done"
+echo "Done deploy script"
