5b1e86d948c7f0301b5c22233759e617867cf22e
Logan <logan@saasquat.ch>
2015-12-14 23:44:00 +0000

    LV: added CDN auto-purge

--- firebaseDeploy.sh
+++ firebaseDeploy.sh
@@ -3,8 +3,11 @@ set -ev
 
 echo "Starting firebase scripts"
 
+## Deploys the `build` directoy automatically
 firebase deploy --token "$FIREBASETOKEN" || echo "Error deploying site"
 
-## TODO: Add MaxCDN purge step: https://github.com/MaxCDN/maxcli/tree/master/maxpurge
+## Purges the entire MaxCDN cache
+npm install maxcdn-purge
+node ./node_modules/maxcdn-purge/purge.js --alias referralsaasquat --key $MAXCDN_KEY  --secret $MAXCDN_SECRET --pull 136632
 
 echo "Done deploy script"
