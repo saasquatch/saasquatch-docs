2f2d5dbc2f9635bef67fceef735f21e97656c6ac
Logan <logan@saasquat.ch>
2015-12-14 19:01:18 +0000

    LV: Added comments

--- firebaseDeploy.sh
+++ firebaseDeploy.sh
@@ -5,4 +5,6 @@ echo "Starting firebase scripts"
 
 firebase deploy --token "$FIREBASETOKEN" || echo "Error deploying site"
 
+## TODO: Add MaxCDN purge step: https://github.com/MaxCDN/maxcli/tree/master/maxpurge
+
 echo "Done deploy script"
