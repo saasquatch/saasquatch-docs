7d268a159005d68100289c8a0d1543856cc35afc
Logan <logan@saasquat.ch>
2016-06-01 22:56:28 +0000

    LV: Make firebase deploy use default

--- firebaseDeploy.sh
+++ firebaseDeploy.sh
@@ -5,7 +5,7 @@ echo "Starting firebase scripts"
 
 ## Deploys the `build` directoy automatically
 npm install -g firebase-tools || { echo "Couldn't install Firebase tools" ; exit 1; }
-firebase deploy --token "$FIREBASETOKEN" || { echo "Error deploying site to Firebase" ; exit 1; }
+firebase use default && firebase deploy --token "$FIREBASETOKEN" || { echo "Error deploying site to Firebase" ; exit 1; }
 
 ## Purges the entire MaxCDN cache
 npm install maxcdn-purge  || { echo "Error installing MaxCDN Purge" ; exit 1; }
