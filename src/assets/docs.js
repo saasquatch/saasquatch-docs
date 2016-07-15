898070f3a9b0f3c4966590ad350ee96987e8a2cf
Logan <logan@saasquat.ch>
2016-07-15 04:22:11 +0000

    LV: Fixed smooth scroll, added scroll selection to side nav

--- src/assets/docs.js
+++ src/assets/docs.js
@@ -6,80 +6,19 @@
 /* global Handlebars */
 /* global smoothScroll */
 
+
+var categories = ["successCenter", "developerCenter", "designerCenter"];
+
 jQuery(document).ready(function() {
-
-    var menuDom = jQuery("#my-menu");
-
-    /**
-     * Sets the current page nav item as "Selected"
-     * 
-     * Source: https://css-tricks.com/snippets/jquery/highlight-all-links-to-current-page/
-     */
-    var selectedOne = false;
-    jQuery("a", menuDom).filter(function(){
-        // TODO: Make anchor-tag pages work
-        // TODO: If no exact page matches, provide a reasonable default...
-        var thisUrl = window.location.pathname.replace(/\/+$/, "");
-        var thatUrl = jQuery(this).attr("href").replace(/\/+$/, "");
-        return thisUrl == thatUrl;
-    })
-    .first()
-    .each(function() {
-        selectedOne = true;
-        jQuery(this).parent("li").addClass("Selected");
-    });
     
-    var categories = ["successCenter", "developerCenter", "designerCenter"];
-
-    if(!selectedOne){
-        // If no nav selected, defaults to the category
-        categories.map(function(category){
-            jQuery("body."+category+ " li."+category).addClass("Selected");
-        });
+    initializeScolling(); // Scrolling is first, because it's a dependency of nav
+    initializeNav();
+    initializeSearch();
+    
+    if ( window.location.hash ) { 
+        smoothScroll.animateScroll( null, window.location.hash);
     }
 
-    
-    /*
-    * Copies all the "categories" classes to their sub-lists.
-    *    This has to be done before MMenu is initalized.
-    */
-    categories.map(function(category){
-        jQuery("li."+category+" ul", menuDom).addClass(category);
-    });
-
-    /**
-     *  Loads the Navigation menu (uses the MMenu jQuery plugin)
-     */
-    menuDom.mmenu({
-         // configuration
-         "offCanvas": false,
-        //  "counters": true,
-         "iconPanels": {
-            "add": true,
-            "visible": 2
-         },
-         extensions: [
-            "theme-squatchdocs",
-            "multiline"
-         ]
-         
-        //  offCanvas: {
-        //     pageSelector: "#my-page",
-        //     pageNodetype: "category"
-        //  }
-      }).init(function($panels){
-        /**
-         * Copies the "category" styling onto their parent `.mm-panel` containers
-         * 
-         */
-        categories.map(function(category){
-            jQuery(".mm-panel > ul." + category, menuDom).parent(".mm-panel").addClass(category);
-        });
-      });
-
-
-    window.myMenu = menuDom.data( "mmenu" );
-      
     jQuery('.js-docs-collapse').each(function() {
         var content = jQuery(this);
         var toggler = jQuery("<a class='js-docs-collapse-toggle'>&nbsp;</a>").click(function(){
@@ -89,42 +28,11 @@ jQuery(document).ready(function() {
         content.before(toggler);
     });
     
-    jQuery('.jsonview').each(function() {
-        var each = jQuery(this);
-        var json = JSON.parse(each.text());
-        Object.keys(json).sort();
-        each.JSONView(json, { collapsed: true });
-    });
-
-
-    hljs.initHighlighting();
-    
     jQuery("#js-location-path").text(document.location.pathname);
 
-
-    // TODO: Implement smooth scroll: https://github.com/cferdinandi/smooth-scroll
-    // TODO: Fix artefact of Affix on chrome
-    smoothScroll.init({
-        speed: 1000,
-        easing: 'easeInOutCubic',
-        offset: 100,
-        updateURL: true,
-        callbackBefore: function ( toggle, anchor ) {},
-        callbackAfter: function ( toggle, anchor ) {
-            anchor.focus();
-        }
-    });
-    if ( window.location.hash ) { 
-     smoothScroll.animateScroll( null, window.location.hash);
-    }
-    
      // Activates smooth scroll links when
-     jQuery("#accordion-scroller .accordion-inner a",
-            "#toc-content a",
-            ".js-faq-list a").click(function(e){
-         var $this = jQuery(this);
-         var anchor = "#" + $this.attr('href').split("#")[1];
-         smoothScroll.animateScroll( null, anchor);
+     jQuery("#toc-content a, .js-faq-list a").click(function(e){
+         smoothScrollTo(jQuery(this));
          return false;
      });
 
@@ -154,8 +62,131 @@ jQuery(document).ready(function() {
         }
     })();
     
+
+    // TODO: LV: Move these highlighting functions server-side to improve page render time    
+    jQuery('.jsonview').each(function() {
+        var each = jQuery(this);
+        var json = JSON.parse(each.text());
+        Object.keys(json).sort();
+        each.JSONView(json, { collapsed: true });
+    });
+    hljs.initHighlighting();
     
+
+});
+
+function initializeScolling(){
+    // TODO: Fix artefact of Affix on chrome
+    smoothScroll.init({
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 100,
+        updateURL: true,
+        callbackBefore: function ( toggle, anchor ) {},
+        callbackAfter: function ( toggle, anchor ) {
+            anchor.focus();
+        }
+    });
+}
+
+function initializeNav(){
+    var menuDom = jQuery("#my-menu");
+
     /**
+     * Sets the current page nav item as "Selected"
+     * 
+     * Source: https://css-tricks.com/snippets/jquery/highlight-all-links-to-current-page/
+     */
+    var foundElement = null;
+    var foundDepth = -1;
+    
+    function found(el, depth){
+        if(depth > foundDepth){
+            foundElement = el;
+            foundDepth = 0;
+        }
+    }
+    jQuery("a", menuDom).each(function(){
+        // TODO: Make anchor-tag pages work
+        // TODO: If no exact page matches, provide a reasonable default...
+        var thisUrl = jQuery(this).attr("href").replace(/\/+$/, "");
+        
+        var thatUrl = window.location.pathname.replace(/\/+$/, "");
+
+        var thatUrlWithHash = window.location.pathname + window.location.hash;
+        var thatUrlWithHashNoSlash = thatUrl + window.location.hash;
+        if(thisUrl == thatUrlWithHash || thisUrl == thatUrlWithHashNoSlash){
+            // Checks for HASH/ANCHOR based mapping first
+            found(jQuery(this).parent("li"), 2);
+            return;
+        }
+        
+        // Checks for that URL minus any trailing slashes
+        if(thisUrl == thatUrl){
+            found(jQuery(this).parent("li"), 1);
+        }
+    });
+    if(!foundElement){
+        // If no nav selected, defaults to the category
+        categories.map(function(category){
+            found(jQuery("body."+category+ " li."+category), 0);
+        });
+    }
+    if(foundElement){
+        foundElement.addClass("Selected");
+    }
+
+    
+    /*
+    * Copies all the "categories" classes to their sub-lists.
+    *    This has to be done before MMenu is initalized.
+    */
+    categories.map(function(category){
+        jQuery("li."+category+" ul", menuDom).addClass(category);
+    });
+
+    /**
+     *  Loads the Navigation menu (uses the MMenu jQuery plugin)
+     */
+    menuDom.mmenu({
+         // configuration
+         "offCanvas": false,
+        //  "counters": true,
+         "iconPanels": {
+            "add": true,
+            "visible": 2
+         },
+         extensions: [
+            "theme-squatchdocs",
+            "multiline"
+         ]
+        //  offCanvas: {
+        //     pageSelector: "#my-page",
+        //     pageNodetype: "category"
+        //  }
+      }).init(function($panels){
+        /**
+         * Copies the "category" styling onto their parent `.mm-panel` containers
+         * 
+         */
+        categories.map(function(category){
+            jQuery(".mm-panel > ul." + category, menuDom).parent(".mm-panel").addClass(category);
+        });
+        jQuery("a.nav-onpage, .nav-onpage a").click(function(){
+            // Trigger this method to set or unset a menu item as "selected".
+            var $li = jQuery(this).parent('li');
+            window.myMenu.setSelected($li);
+            smoothScrollTo(this);
+            return false;
+        });
+      });
+
+
+    window.myMenu = menuDom.data( "mmenu" );
+}
+
+function initializeSearch(){
+        /**
      * 
      *  Docs Search
      * 
@@ -219,11 +250,18 @@ jQuery(document).ready(function() {
         });
 
     });
-});
+}
 
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(window.location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// Extracts the HREF value, and smooth scrolls to that location.
+function smoothScrollTo(elem){
+    var $this = jQuery(elem);
+    var anchor = "#" + $this.attr('href').split("#")[1];
+    smoothScroll.animateScroll(null, anchor);
 }
