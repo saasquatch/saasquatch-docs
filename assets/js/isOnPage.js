d41d2e2eb8ef58e425b73bdabbd252bd6f22fda0
Logan <logan.volkers@yupiq.com>
2017-03-03 22:16:53 +0000

    LV: fix tests

--- assets/js/isOnPage.js
+++ assets/js/isOnPage.js
@@ -3,7 +3,7 @@
  * Detects if two HREFs are on teh same page. Helpful for Anchor detection and same-page scrolling
  * 
  */
-export default function isOnPage(thisPage, thatPage) {
+module.exports = function isOnPage(thisPage, thatPage) {
     var thisUrl = thisPage.split("#")[0].replace(/\/+$/, "");
     var thatUrl = thatPage.split("#")[0].replace(/\/+$/, "");
     return thisUrl == thatUrl;
