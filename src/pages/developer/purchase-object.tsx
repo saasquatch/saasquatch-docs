f39b830b28e12709ede4c15b39afc117de4d82de
Logan Volkers <logan@saasquat.ch>
2019-11-19 10:05:07 -0800

    LV: Fix purchase event

--- src/pages/developer/purchase-object.tsx
+++ src/pages/developer/purchase-object.tsx
@@ -1,51 +1,48 @@
 import React from "react";
 
-import { Properties } from "../../components/Properties";
-
 import PurchaseEvent from "@saasquatch/schema/json/PurchaseEventFields.schema.json";
 
-// ---
-// title: Growth Automation Purchase Event
-// highlights: Details about the SaaSquatch User Purchase Event.
-// slug: developer/purchase-object
-// sectionType: successArticle
-// template: pages/purchaseObject.html
-// ---
+import { Properties } from "../../components/Properties";
+import PageHeader from "../../components/PageHeader";
 
-/**
- *
- * 
-{% import "../macros/swaggerMacros.html" as swaggerMacros %}
-*/
+const entry = {
+  title: "Growth Automation Purchase Event",
+  highlights: "Details about the SaaSquatch User Purchase Event.",
+  slug: "developer/purchase-object",
+  sectionType: "successArticle",
+  template: "pages/purchaseObject.html"
+};
 
 export default function render() {
   return (
-    <>
-      <p>
-        The SaaSquatch system is configured to accept being sent a wide range of{" "}
-        <a href="/growth/ga-mechanisms/#user-events">event data</a> about
-        actions users take within your product. This event information will
-        power the programs within your SaaSquatch project, and can be used to{" "}
-        <a href="/growth/ga-mechanisms/#program-rules">trigger</a> sending
-        emails and providing rewards.
-      </p>
+    <PageHeader {...entry}>
+      <>
+        <p>
+          The SaaSquatch system is configured to accept being sent a wide range
+          of <a href="/growth/ga-mechanisms/#user-events">event data</a> about
+          actions users take within your product. This event information will
+          power the programs within your SaaSquatch project, and can be used to{" "}
+          <a href="/growth/ga-mechanisms/#program-rules">trigger</a> sending
+          emails and providing rewards.
+        </p>
 
-      <p>
-        Events can be sent to SaaSquatch through our{" "}
-        <a href="/api/methods/#trackEvent">REST API</a> or squatch.js Javascript
-        Library.
-      </p>
+        <p>
+          Events can be sent to SaaSquatch through our{" "}
+          <a href="/api/methods/#trackEvent">REST API</a> or squatch.js
+          Javascript Library.
+        </p>
 
-      <p>
-        The `puchase` event is a reserved user event type within the SaaSquatch
-        system, and requires information to be sent to SaaSquatch in a defined
-        format. This format is as follows:
-      </p>
+        <p>
+          The `puchase` event is a reserved user event type within the
+          SaaSquatch system, and requires information to be sent to SaaSquatch
+          in a defined format. This format is as follows:
+        </p>
 
-      <h4>Fields</h4>
-      <div>
-        <Properties schema={PurchaseEvent.properties} />
-      </div>
-    </>
+        <h4>Fields</h4>
+        <div>
+          <Properties schema={PurchaseEvent} />
+        </div>
+      </>
+    </PageHeader>
   );
 }
