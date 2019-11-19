22f0bd7f2fddd7bbce6c9ad82a5a4d27836a1201
Logan Volkers <logan@saasquat.ch>
2019-11-18 21:31:42 -0800

    LV: Plugin for React-static webpack

---
+++ plugins/saasquatch-webpack/node.api.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 
+ * Allows webpack customization (used to be part of core react-static, but not anymore)
+ */
+export default ({ externals }) => {
+  return {
+    webpack: (config, { stage }) => {
+      if (stage === "prod") {
+        config.externals = externals;
+      } else if (stage === "dev") {
+      }
+      return config;
+    }
+  };
+};
