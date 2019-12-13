1dd4c99274d8bc130a0146ca92e5cffb1f6ce041
Logan Volkers <logan@saasquat.ch>
2019-12-13 11:39:45 -0800

    Deprecated and Classic tags for integrations (#92)
    * Update IntegrationLander.tsx

--- src/containers/IntegrationLander.tsx
+++ src/containers/IntegrationLander.tsx
@@ -15,7 +15,8 @@ export default function render() {
     info,
     moreInfo,
     articleContent,
-    keyFeatures
+    keyFeatures,
+    tags
   } = entry;
 
   let logoUrl;
@@ -48,6 +49,8 @@ export default function render() {
           </div>
         </div>
 
+        {tags && tags.some(t=>t==="deprecated") && <div className="alert alert-warning">This integration has been <b>deprecated</b>. It will continue working for existing customers, but is not available to be installed by new customers.</div>}
+        {tags && tags.some(t=>t==="classic-only") && <div className="alert alert-warning"><span className="label label-default">Classic Only</span> This integration only works with Classic referral programs. It is not available to be installed by <a href="https://docs.referralsaasquatch.com/growth/saasquatch-ga">Growth Automation</a> customers.</div>}
         {!articleContent && <div className="row-fluid">
           <div className="span7">
             <h3>Key features</h3>
