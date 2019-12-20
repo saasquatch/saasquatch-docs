9b81814cfcfe1c28c070244192f657c89dd8e194
Logan Volkers <logan@saasquat.ch>
2019-12-19 18:16:55 -0800

    LV: Add layoutEffect for more graceful rendering

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,71 +1,94 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 // @ts-ignore no types for marked
 import marked from "marked";
-import styled from "styled-components"
-import uuid from "uuid";
+import styled from "styled-components";
 
 let mermaidAPI;
 if(typeof document === "undefined"){
   mermaidAPI = null;
 }else{
-  mermaidAPI = require("mermaid").mermaidAPI;
+  mermaidAPI = require("mermaid").mermaidAPI
 }
-
 // Get reference
 const renderer = new marked.Renderer();
 
 const MD = styled.div`
-.heading-anchor{
-  font-style: normal;
-  font-variant: normal;
-  font-weight: normal;
-  font-stretch: normal;
-  font-size: 0.75em;
-  line-height: inherit;
-  font-family: anchorjs-icons;
-  position: absolute;
-  margin-left: -1.5em;
-  padding-right: 0.5em;
-  opacity: 0;
-  -webkit-font-smoothing: antialiased;
-  text-decoration: none;
+  .heading-anchor {
+    font-style: normal;
+    font-variant: normal;
+    font-weight: normal;
+    font-stretch: normal;
+    font-size: 0.75em;
+    line-height: inherit;
+    font-family: anchorjs-icons;
+    position: absolute;
+    margin-left: -1.5em;
+    padding-right: 0.5em;
+    opacity: 0;
+    -webkit-font-smoothing: antialiased;
+    text-decoration: none;
+  }
+
+  *:hover > .heading-anchor,
+  .heading-anchor:focus {
+    opacity: 1;
+  }
+  .mermaid {
+    visibility: collapse; /** Hides text until the graph is rendered */
+  }
+  .mermaid * {
+    visibility: visible;
+  }
+  .mermaid svg {
+    margin: 0 auto;
+    display: block;
+  }
+  .mermaid svg .label {
+    text-shadow: none; /* Overrides bootstrap CSS class conflict*/
+  }
+`;
+
+function useMermaid() {
+  const ref = useRef(null);
+  useLayoutEffect(() => {
+    if (ref && ref.current && mermaidAPI) {
+      const charts = ref.current.querySelectorAll(".mermaid");
+      charts.forEach(element => {
+        if(element.querySelector("svg")){
+          return; // Already rendered
+        }
+        var insertSvg = function(svgCode:string, bindFunctions) {
+          element.innerHTML = svgCode;
+        };
+        try{
+          var graphDefinition = element.innerText;
+          mermaidAPI.render("graphDiv", graphDefinition, insertSvg);  
+        }catch(e){
+          // Ignore errors
+        }
+      });
+    }
+  });
+  return [ref];
 }
 
-*:hover > .heading-anchor, .heading-anchor:focus {
-  opacity: 1;
-}
-.mermaid-center svg{
-  margin: 0 auto;
-  display: block;
-}
-.mermaid-center svg .label{
-  text-shadow: none; /* Overrides bootstrap CSS class conflict*/
-}
-`
-
-renderer.code = function (code?:string, language?:string) {
-  if(language && language.toLowerCase() === "mermaid"){
-    if(typeof document === "undefined"){
-      // Don't render mermaid server-side. It's not friendly / requires puppeteer. (see: https://github.com/mermaid-js/mermaid/issues/593)
-      return '<pre><code class="lang-' + language + '">'+code+'</code></pre>';
-    }
-    try{
-      const svg = mermaidAPI.render(uuid(), code);
-      return '<div class="mermaid-center">'+svg+'</div>';
-    }catch(e){
-      console.error("Error in rendering chart", e, code);
-      return '<pre><code class="lang-' + language + '">'+code+'</code></pre>';
-    }
+renderer.code = function(code?: string, language?: string) {
+  if (language && language.toLowerCase() === "mermaid") {
+    return '<div class="mermaid">' + code + "</div>";
   } else {
-      return '<pre><code class="lang-' + language + '">'+code+'</code></pre>';
+    return '<pre><code class="lang-' + language + '">' + code + "</code></pre>";
   }
 };
 
 // TODO: add anchor- &#x1F517;
 // Override function
-renderer.heading = function (text:string, level:number, raw:string, slugger:any)
-{
-  const slug = raw.toLowerCase().replace(/[^\w]+/g, '-');
+renderer.heading = function(
+  text: string,
+  level: number,
+  raw: string,
+  slugger: any
+) {
+  const slug = raw.toLowerCase().replace(/[^\w]+/g, "-");
   // TODO: Upgade to newer Marked for better slug support. Newer slugger supports duplicate slugs better by adding numbers (e.g. `head`, `head1`, `head2`)
   // const slug = slugger.slug(raw);
 
@@ -76,10 +99,11 @@ renderer.heading = function (text:string, level:number, raw:string, slugger:any)
           </h${level}>`;
 };
 
-export default function Markdown({source}:{source:string}){
-    if(!source) return <div/>
-    
-    var rawMarkup = marked(source, {sanitize: false, renderer: renderer});
-    const html =  { __html: rawMarkup };
-    return <MD dangerouslySetInnerHTML={html} />
+export default function Markdown({ source }: { source: string }) {
+  if (!source) return <div />;
+  const [ref]= useMermaid();
+
+  var rawMarkup = marked(source, { sanitize: false, renderer: renderer });
+  const html = { __html: rawMarkup };
+  return <MD dangerouslySetInnerHTML={html} ref={ref} />;
 }
