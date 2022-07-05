7154b81d4363ce121eb74d7ffbd240b317339c03
Madeline Solis de Ovando <madeline.sdeo@referralsaasquatch.com>
2022-07-04 17:01:45 -0700

    MS: refactored core category & fixed core spacing

--- src/navigation/CoreCategoryView.tsx
+++ src/navigation/CoreCategoryView.tsx
@@ -14,8 +14,15 @@ const TitleArrowDiv = styled.div`
   display: flex !important;
   justify-content: space-between !important;
 `;
+
+const IconTitleDiv = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 18px;
+`;
 export const CoreCategoryView = (props: {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   to: string;
   title: string;
   icon: SVGProps;
@@ -25,25 +32,31 @@ export const CoreCategoryView = (props: {
     <li ref={props.parentRef}>
       {/* <a class="mm-next" href="#mm-3" goes here, sibling of CoreCategoryLink */}
       <CoreCategoryLink to={props.to}>
-        <SidebarSVG {...props.icon} />
-        {props.title}
-        <SidebarSVG
-          width="35%"
-          viewBox="0 0 8 13"
-          d="M0 1.91L4.58 6.5L0 11.09L1.41 12.5L7.41 6.5L1.41 0.5L0 1.91Z"
-        />
+        <IconTitleDiv>
+          <SidebarSVG {...props.icon} />
+          {props.title}
+        </IconTitleDiv>
+        {props.children && (
+          <SidebarSVG
+            width="35%"
+            viewBox="0 0 8 13"
+            d="M0 1.91L4.58 6.5L0 11.09L1.41 12.5L7.41 6.5L1.41 0.5L0 1.91Z"
+          />
+        )}
       </CoreCategoryLink>
-      <ul>
-        <li>
-          <TitleLink to={props.to}>
-            {" "}
-            <SidebarSVG {...props.icon} width="70%" />
-            {props.title}
-          </TitleLink>
-        </li>
-        <DivideLineLi />
-        {props.children}
-      </ul>
+      {props.children && (
+        <ul>
+          <li>
+            <TitleLink to={props.to}>
+              {" "}
+              <SidebarSVG {...props.icon} width="70%" />
+              {props.title}
+            </TitleLink>
+          </li>
+          <DivideLineLi />
+          {props.children}
+        </ul>
+      )}
     </li>
   );
 };
