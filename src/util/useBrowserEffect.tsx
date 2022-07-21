7fb13a9c1437d38c66eb620391ab9704fc19f675
Johan Venter <johan@saasquatch.com>
2022-07-21 09:12:59 -0700

    Remove the debugging

--- src/util/useBrowserEffect.tsx
+++ src/util/useBrowserEffect.tsx
@@ -4,9 +4,8 @@ import { DependencyList, EffectCallback, useLayoutEffect } from "react";
  *  Source: https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a
  */
 
-
 function noOpEffect(effect: EffectCallback, deps?: DependencyList) {
-  console.debug("No effect called", effect, deps);
+  // console.debug("No effect called", effect, deps);
 }
 
 const useBrowserEffect =
