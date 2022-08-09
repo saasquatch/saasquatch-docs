17ed2b6bfcdb72a954d57a3d6ddfff20dd6f3c91
Keegan Kavanagh <keegankavanagh@live.com>
2022-08-09 16:44:25 -0700

    Just testing

--- static.config.js
+++ static.config.js
@@ -150,7 +150,7 @@ export default {
     dist: "build", // The production output directory.
     // devDist: "tmp/dev-server", // The development scratch directory.
     // public: "public", // The public directory (files copied to dist during build)
-    assets: "build", // The output directory for bundled JS and CSS
+    // assets: "build", // The output directory for bundled JS and CSS
     // buildArtifacts: "artifacts" // The output directory for generated (internal) resources
   },
   devServer: {
