503b2cc2c2e626df5f64d4caf781dca5a40516d8
Truman <truman.gao@referralsaasquatch.com>
2022-09-23 14:33:51 -0700

    Update TOC styles through wrapped styled component

--- src/components/TocFrame.tsx
+++ src/components/TocFrame.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 //@ts-ignore
 import { StickyContainer, Sticky } from "react-sticky";
+import styled from "styled-components";
 
 import PageHeader from "./PageHeader";
 import TOC from "./TOC";
+
 
 /**
  * A document / markdown / contenful entry
@@ -23,6 +25,47 @@ export type Entry = {
   fields?: { seoDescription?: string };
 };
 
+
+// get from gobal color somewhere
+const TOCStylesDiv = styled.div`
+  ul{
+    margin: 0;
+  }
+  li > ul{
+    margin-top: 5px
+    margin-left: 12px
+  }
+  li{
+    list-style-type: none;
+    line-style:none;
+
+    border-left: 1px solid #E2E2E2;
+  }
+
+  /* clear default <a> styles*/
+  a, a:link, a:visited, a:hover, a:active {
+    color: inherit;
+    text-decoration: inherit;
+    font-weight: inherit;
+  }
+  a {
+    display: inline-block
+    padding: 8px 12px;
+
+    font-family: 'Helvetica';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    color: #575757;
+  }
+  a:hover {
+    font-weight: 700;
+    border-left: 2px solid #007A5B;
+    padding-right: 0;
+  }
+`
+
+
 export default function render({
   entry,
   children,
@@ -34,6 +77,7 @@ export default function render({
 }) {
   return (
     <StickyContainer>
+      {console.log("entry",entry)}
       <div className="row-fluid">
         <div className="span9">
           <PageHeader {...entry} children={children}/>
@@ -46,12 +90,17 @@ export default function render({
                   <div className="hidden-phone">
                     <div className="toc-title">In this Article:</div>
 
-                    {entry.contents && <TOC source={entry.contents} />}
-                    <ul>
-                      <li>
-                        <a href="#top">Introduction</a>
-                      </li>
-                    </ul>
+                    <TOCStylesDiv>
+                      <ul>
+                        <li>
+                          <a href="#top">Introduction</a>
+                        </li>
+                      </ul>
+                      
+                      {entry.contents && <TOC source={entry.contents} />}
+
+                    </TOCStylesDiv>
+
                   </div>
                 </div>
               );
